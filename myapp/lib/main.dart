4ef60a7d501e100aa83cd2d3a191b9c6630e228a
gadsdev <gusttavo212@gmail.com>
2019-09-29 18:49:34 -0300

    Styled Widget

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MaterialApp(
-    title: 'wins counter league of legends',
-    home: new Container(
-      color: Color.fromARGB(255, 255, 255, 255),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('Hello World, dart!'),
-          Text('#animado')
-        ],
-      )
-    )
-  ));
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'wins counter league of legends',
+        home: new Container(
+            color: Color.fromARGB(255, 255, 255, 255),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Hello World, dart!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.blueAccent,
+                        decoration: TextDecoration.none)),
+                Text('#Gustavo',
+                    style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 30,
+                        decoration: TextDecoration.none))
+              ],
+            )));
+  }
 }
