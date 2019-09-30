df4ca38be42e27c7f9cc6f775f74066f1e009ee6
gadsdev <gusttavo212@gmail.com>
2019-09-29 22:45:45 -0300

    Refactoring to path organization

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'template.dart';
+
 void main() {
   runApp(new MyApp());
 }
@@ -7,47 +9,7 @@ void main() {
 class MyApp extends StatelessWidget {
   Widget build(BuildContext contex) {
     return new MaterialApp(
-        title: 'SON contador de cursos', home: new HomeApp());
-  }
-}
-
-class HomeApp extends StatefulWidget {
-  @override
-  _HomeAppState createState() => _HomeAppState();
-}
-
-class _HomeAppState extends State<HomeApp> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Gustavo Alexandre'),
-      ),
-      body: Container(
-        color: Colors.blueGrey,
-        constraints: BoxConstraints.expand(),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hello World',
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.lightBlueAccent, fontSize: 60),
-            ),
-            Text(
-              '#Animado',
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.blue, fontSize: 20),
-            )
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
-          BottomNavigationBarItem(icon: Icon(Icons.school), title: Text('School'))
-        ],
-      ),
-    );
+        title: 'SON contador de cursos', 
+        home: new Template());
   }
 }
