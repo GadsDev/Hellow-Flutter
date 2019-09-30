1c206212915560b906fde3f21fd427dc18f97bf9
gadsdev <gusttavo212@gmail.com>
2019-09-29 22:53:29 -0300

    Create Second Page

---
+++ myapp/lib/pages/gustavoPage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class GustavoPage extends StatefulWidget {
+  @override
+  _GustavoPageState createState() => _GustavoPageState();
+}
+
+class _GustavoPageState extends State<GustavoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text('Visite: ', textAlign: TextAlign.center,),
+        RaisedButton(
+          child: Text('https://github.com/GadsDev', textAlign: TextAlign.center,),)
+      ],
+    );
+  }
+}
