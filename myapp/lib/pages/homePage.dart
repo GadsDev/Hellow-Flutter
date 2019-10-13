787085b7105f66e94e6915bf38dddec4d2c5e14a
gadsdev <gusttavo212@gmail.com>
2019-10-13 20:29:25 -0300

    Add Photos

--- myapp/lib/pages/homePage.dart
+++ myapp/lib/pages/homePage.dart
@@ -7,12 +7,22 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _animacao = 0;
-
+  int _selectedImage = 0;
+ var _images = [
+   'images/1.jpg',
+   'images/2.jpg',
+   'images/3.jpg',
+   'images/4.png',
+  ' images/5.png',
+   'images/6.png',
+   'images/7.png',
+ ];
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
+        Image.asset(_images[_animacao], height: 150.0,),
         Text('Olá Amigo',
             textAlign: TextAlign.center,
             style: Theme.of(context).textTheme.display3),
@@ -26,8 +36,12 @@ class _HomePageState extends State<HomePage> {
             padding: EdgeInsets.all(20),
             child: FloatingActionButton(
               onPressed: () {
-                setState(() {
-                  _animacao++;
+                setState(() {                   
+                  if(_animacao == 3){
+                    _animacao = 0;
+                  }else{
+                    _animacao++;  
+                  }                  
                 });
               },
               child: Icon(Icons.sentiment_satisfied),
--- myapp/lib/template.dart
+++ myapp/lib/template.dart
@@ -10,7 +10,7 @@ class Template extends StatefulWidget {
 class _TemplateState extends State<Template> {
   var homePage = new HomePage();
    var gustavoPage = new GustavoPage();
-  var _selectedPage = 1;
+  var _selectedPage = 0;
   var _pages;
 
   @override
